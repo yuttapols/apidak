ef3c659084d0851aa69be01a36b6939196a153b8
Yuttapol <myprofile.yuttapol@gmail.com>
2024-05-26 16:27:01 +0700

    create Project

---
+++ src/main/java/com/ws/apidak/ApidakApplication.java
@@ -0,0 +1,13 @@
+package com.ws.apidak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApidakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApidakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ws/apidak/ApidakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ws.apidak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApidakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
